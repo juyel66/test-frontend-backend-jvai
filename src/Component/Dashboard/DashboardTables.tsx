0e3746eed30c21d86a6335b35823934fa5f602e7
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-08-18 02:52:11 +0600

    (updated) dashboard table page add style

--- src/Component/Dashboard/DashboardTables.tsx
+++ src/Component/Dashboard/DashboardTables.tsx
@@ -1,84 +1,10 @@
-import React from "react"
-import { DataTable } from "../Tables/data-table"
-import { columns } from "../Tables/columns"
+import { DashboardTablesPage } from "./DashboardTablesPage";
 
 
-
-async function getData(): Promise<Payment[]> {
-  return [
-    {
-      id: "728ed52f",
-      AUTHOR: "John Doe",
-      FUNCTION: "Manager",
-      STATUS: "ONLINE",
-      EMPLOYED: "13/23/2023",
-      ACTION: "DELETE",
-      email: "m@example.com",
-    },
-    {
-      id: "12a34bc",
-      AUTHOR: "Alice Smith",
-      FUNCTION: "Programmer",
-      STATUS: "OFFLINE",
-      EMPLOYED: "13/23/2024",
-      ACTION: "DELETE",
-      email: "a@example.com",
-    },
-    {
-      id: "12a34bc",
-      AUTHOR: "Alice Smith",
-      FUNCTION: "Programmer",
-      STATUS: "ONLINE",
-      EMPLOYED: "13/23/2024",
-      ACTION: "DELETE",
-      email: "a@example.com",
-    },
-    {
-      id: "12a34bc",
-      AUTHOR: "Alice Smith",
-      FUNCTION: "Programmer",
-      STATUS: "OFFLINE",
-      EMPLOYED: "13/23/2024",
-      ACTION: "DELETE",
-      email: "a@example.com",
-    },
-    {
-      id: "12a34bc",
-      AUTHOR: "Alice Smith",
-      FUNCTION: "Programmer",
-      STATUS: "OFFLINE",
-      EMPLOYED: "13/23/2024",
-      ACTION: "DELETE",
-      email: "a@example.com",
-    },
-    {
-      id: "12a34bc",
-      AUTHOR: "Alice Smith",
-      FUNCTION: "Programmer",
-      STATUS: "OFFLINE",
-      EMPLOYED: "13/23/2024",
-      ACTION: "DELETE",
-      email: "a@example.com",
-    },
-  ]
-}
-
-
-const DashboardTables: React.FC = () => {
-  const [data, setData] = React.useState<Payment[]>([])
-
-  React.useEffect(() => {
-    getData().then(setData)
-  }, [])
-
+export default function DashboardTables() {
   return (
-    <div className="container mx-auto h-screen py-10 ">
-      <h1 className="text-xl  font-bold mb-5">Payments Table</h1>
-      <div className="">
-        <DataTable columns={columns} data={data} />
-      </div>
-    </div>
+    <main className="mt-5 ">
+      <DashboardTablesPage />
+    </main>
   )
 }
-
-export default DashboardTables
