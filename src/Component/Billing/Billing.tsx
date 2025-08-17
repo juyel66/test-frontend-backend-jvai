e3c987855ac88f98d190e418ae3cdf8fb8565669
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-08-18 02:59:10 +0600

    (update) style changed

--- src/Component/Billing/Billing.tsx
+++ src/Component/Billing/Billing.tsx
@@ -2,7 +2,7 @@
 
 const Billing = () => {
     return (
-        <div className="text-white text-3xl text-center h-screen  flex justify-center items-center">
+        <div className=" text-3xl text-red-500 text-center h-screen  flex justify-center items-center">
                This is a billing pages
             
         </div>
